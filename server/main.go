35cd8a02e5664559f44ff10a951c7ae840694416
glens <wanjineng@dingtalk.com>
2020-01-17 18:05:18 +0800

    s

--- server/main.go
+++ server/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/micro/go-micro/config"
+	"github.com/micro/go-micro/config/source/env"
+	"github.com/micro/go-micro/config/source/file"
+	"github.com/micro/go-micro/config/source/flag"
 
 	micro "github.com/micro/go-micro"
 	proto "github.com/chat/greeter"
@@ -15,11 +19,55 @@ func (g *Greeter) Hello(ctx context.Context, req *proto.Request, rsp *proto.Resp
 	return nil
 }
 
+type Host struct {
+	Address string `json:"address"`
+	Port int `json:"port"`
+}
+
+type Config struct {
+	Hosts map[string]Host `json:"hosts"`
+}
+
+var (
+
+	host Host
+)
+
 func main() {
 	// Create a new service. Optionally include some options here.
 	service := micro.NewService(
 		micro.Name("greeter"),
 	)
+	//conf:=config.NewConfig()
+
+	//config.LoadFile("../config/database.json")
+
+	//config.Scan(&conf)
+
+	config.Load(
+		env.NewSource(),
+		flag.NewSource(),
+		file.NewSource(
+			file.WithPath("../config/database.json"),
+		),
+	)
+
+
+	config.Get("hosts","database").Scan(&host)
+
+	host11:=config.Get("hosts","database","address").String("localhost")
+	port11:=config.Get("hosts","database","port").Int(3000)
+
+	fmt.Println("=====================")
+	fmt.Printf("%#v\n",host)
+	fmt.Println(host11)
+	fmt.Println(port11)
+	//fmt.Printf("%#v\n",conf)
+	fmt.Println("======================")
+
+
+
+
 
 	// Init will parse the command line flags.
 	service.Init()
